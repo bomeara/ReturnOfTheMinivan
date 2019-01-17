8e75d7da4b4d25420ee51f784359b0cf14782f2b
Brian OMeara <omeara.brian@gmail.com>
2019-01-17 17:23:39 -0500

    updated with all names

--- R/plan.R
+++ R/plan.R
@@ -7,5 +7,8 @@ my_plan_immediately <- drake_plan(
   references.df = GetAllReferences(),
   cache_all = CacheAllPDFsImmediately(references.df),
   all_pdfs = CacheRemainingPDFs(cache_all),
-  all_names = GetAllNames(all_pdfs)
+  all_names = GetAllNames(all_pdfs),
+  save_all_names = save(all_names, file="all_names.rda"),
+  all_taxonomy = GetAllTaxonomy(all_names),
+  save_all_taxonomy = save(all_taxonomy, file="all_taxonomy.rda")
 )
