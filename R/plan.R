0b3627af79ce946984c613fb71d96307e21ab4cd
Brian OMeara <omeara.brian@gmail.com>
2019-01-18 09:14:23 -0500

    all names processed

--- R/plan.R
+++ R/plan.R
@@ -1,3 +1,5 @@
+pkgconfig::set_config("drake::strings_in_dots" = "literals")
+
 my_plan <- drake_plan(
   references.df = GetAllReferences(),
   references.txt = DownloadAndExtractAllPDFs(references.df)
@@ -8,7 +10,8 @@ my_plan_immediately <- drake_plan(
   cache_all = CacheAllPDFsImmediately(references.df),
   all_pdfs = CacheRemainingPDFs(cache_all),
   all_names = GetAllNames(all_pdfs),
-  save_all_names = save(all_names, file="all_names.rda"),
+  save_all_names = save(all_names, file=file_out('all_names.rda')),
   all_taxonomy = GetAllTaxonomy(all_names),
-  save_all_taxonomy = save(all_taxonomy, file="all_taxonomy.rda")
+  save_all_taxonomy = save(all_taxonomy, file=file_out('all_taxonomy.rda')),
+  save_all_taxonomy_csv = write.csv(all_taxonomy$resolved, file=file_out('all_taxonomy.csv'))
 )
